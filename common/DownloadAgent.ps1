0fe6235cf1c9953d122ddcc0f776d6fad82e2b26
Georg Drobny <georgd@microsoft.com>
2022-12-20 16:43:10 +0100

    Added GitIgnore, launch.json and new functionality for script.

--- common/DownloadAgent.ps1
+++ common/DownloadAgent.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 7
+
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
@@ -5,55 +7,105 @@ param(
     [Parameter(Mandatory=$true)]
     [string] $AzureDevopsPersonalAccessToken,
     [Parameter(Mandatory=$true)]
-    [string] $DestinationDirectory
+    [string] $DestinationDirectory,
+    [string] $CustomAgentLocation
 )
 
 enum Platform { 
     Unknown
     Linux
-    MacOS
-    Windows
+    Osx
+    Win
 }
 
-[string] $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $AzureDevopsPersonalAccessToken)))
-[hashtable] $AuthHeader = @{ Authorization = "Basic $base64AuthInfo" }
-[string] $Uri = [string]::Empty
+Function Get-LatestAgent
+{
+    Param(
+        [string] $AzureDevopsOrganization,
+        [string] $AzureDevopsPersonalAccessToken,
+        [Platform] $Platform
+    )
+
+    [string] $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $AzureDevopsPersonalAccessToken)))
+    [hashtable] $AuthHeader = @{ Authorization = "Basic $base64AuthInfo" }
+    [string] $Uri = [string]::Empty
+    
+    switch ($Platform) {
+        Linux { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=linux-x64" }
+        Osx { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=osx-x64" }
+        Win { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=win-x64" }
+        Default {
+            Write-Error "Invalid Platform, exiting!"
+            exit 1
+        }    
+    }
+    
+    Write-Host "Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
+    $rsp = Invoke-WebRequest -uri $Uri -UseBasicParsing -Headers $AuthHeader
+    if ($rsp.StatusCode -ne 200) {
+        Write-Error "Could not determine a matching Azure Pipelines agent - check that account 'AzureDevopsOrganization' is correct and the token is valid for that account"
+        exit 2
+    }
+
+    [PSCustomObject] $agentList = ConvertFrom-Json $rsp.content;
+
+    return [pscustomobject]@{
+        "filename" = $agentList.value[0].filename
+        "version" = "{0}.{1}.{2}" -f $agentList.value[0].version.major, $agentList.value[0].version.minor, $agentList.value[0].version.patch
+        "downloadUrl" = $agentList.value[0].downloadUrl
+    }
+}
+
+Function Get-CustomAgent
+{
+    Param(
+        [string] $CustomAgentLocation,
+        [Platform] $Platform
+    )
+
+    $loc = $CustomAgentLocation -as [uri]
+    if ($loc -eq $Null) {
+        Write-Error "Invalid custom agent location $CustomAgentLocation! exiting!"
+        exit 3
+    }
+    if ($loc.Segments[-1] -notmatch "^vsts-agent-(?<platform>\w+)-(?<arch>\w+)-(?<version>\d+.\d+.\d+)") {
+        Write-Error "Invalid custom agent location $CustomAgentLocation! exiting!"
+        exit 3
+    }
+    if ($matches["platform"] -ne $Platform) {
+        Write-Error "Invalid custom agent platform $CustomAgentLocation! exiting!"
+        exit 3
+    }
+
+    return [pscustomobject]@{
+        "filename" = $loc.Segments[-1]
+        "version" = $matches["version"]
+        "downloadUrl" = $CustomAgentLocation
+    }
+}
+
 [Platform] $Platform = [Platform]::Unknown
-
 if ($IsLinux) { $Platform = [Platform]::Linux }
-elseif ($IsMacOS) { $Platform = [Platform]::MacOS }
-elseif ($IsWindows) { $Platform = [Platform]::Windows }
+elseif ($IsMacOS) { $Platform = [Platform]::Osx }
+elseif ($IsWindows) { $Platform = [Platform]::Win }
 
-switch ($Platform) {
-    Linux { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=linux-x64" }
-    MacOS { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=osx-x64" }
-    Windows { $Uri = "$AzureDevopsOrganization/_apis/distributedtask/packages/agent?platform=win-x64" }
-    Default {
-        Write-Error "Invalid Platform, exiting!"
-        exit 1
-    }    
+[pscustomobject] $agent = $Null
+if ([string]::IsNullOrEmpty($CustomAgentLocation)) {
+    $agent = Get-LatestAgent $AzureDevopsOrganization $AzureDevopsPersonalAccessToken $Platform
+}
+else {
+    $agent = Get-CustomAgent $CustomAgentLocation $Platform
 }
 
-Write-Host "Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
-$rsp = Invoke-WebRequest -uri $Uri -UseBasicParsing -Headers $AuthHeader
-if ($rsp.StatusCode -ne 200) {
-    Write-Error "Could not determine a matching Azure Pipelines agent - check that account 'AzureDevopsOrganization' is correct and the token is valid for that account"
-    exit 2
-}
-
-[PSCustomObject] $agentList = ConvertFrom-Json $rsp.content;
-[string] $agentPackage = Join-Path $DestinationDirectory $agentList.value[0].filename
-[string] $agentVersion =  "{0}.{1}.{2}" -f $agentList.value[0].version.major, $agentList.value[0].version.minor, $agentList.value[0].version.patch
-
+[string] $agentPackage = Join-Path $DestinationDirectory $agent.filename
 if (!(Test-Path $DestinationDirectory -ErrorAction SilentlyContinue)) {
     new-Item -ItemType Directory -Path $DestinationDirectory | Out-Null
 }
 
 $ProgressPreference = 'SilentlyContinue'
-
-Write-Host "Downloading Azure Pipelines agent for $Platform Version $AgentVersion ..." -ForegroundColor Cyan
+Write-Host "Downloading Azure Pipelines agent for $Platform Version $($agent.version) ..." -ForegroundColor Cyan
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -UseBasicParsing -Uri $agentList.value[0].downloadUrl -OutFile $agentPackage
+Invoke-WebRequest -UseBasicParsing -Uri $agent.downloadUrl -OutFile $agentPackage
 if ($IsMacOS -or $IsWindows) { Unblock-File $agentPackage }
 Write-Host "Finshed Downloading Azure Pipelines agent" -ForegroundColor Green
 Return $agentPackage
